4e090c7592dd096ec66d5adee29c169f2b5ef4b6
ToxicNerd <k.lehenbauer13@gmail.com>
2019-10-03 16:30:58 -0700

    Wrote logic to add user input to Firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+// Set up Firebase
+let firebaseConfig = {
+    apiKey: "AIzaSyB8EzaW9n1pK5Zf1Gtt2HGXDn9oBjJv1Q4",
+    authDomain: "train-scheduler-4515b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4515b.firebaseio.com",
+    projectId: "train-scheduler-4515b",
+    storageBucket: "",
+    messagingSenderId: "557159151534",
+    appId: "1:557159151534:web:69be4b1264f684131efa3f"
+  };
+
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+   // Assign Firebase data to variable
+   let database = firebase.database();
+
+   // Click event to add new train data to database
+   $("#add-train-btn").on("click", function(event) {
+        event.preventDefault();
+
+        // Grab data from user input & assign to variables
+        let trainName = $("#train-name-input").val().trim();
+        let destination = $("#destination-input").val().trim();
+        let firstTrain = $("#first-train-input").val().trim();
+        let frequency = $("#frequency-input").val().trim();
+
+        // Assign this data to an object that can be added to database
+        let newTrain = {
+            name: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency
+        };
+
+        // Push new object to database
+        database.ref().push(newTrain);
+
+        // Clear input fields
+        $("#train-name-input").val("");
+        $("#destination-input").val("");
+        $("#first-train-input").val("");
+        $("#frequency-input").val("");
+   })
+
+})
